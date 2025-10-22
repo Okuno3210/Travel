9edc8813261f26828ae2b725e8db7273f1eb6a3e
3030931 <3030931@30309-31>
2025-10-22 10:46:25 +0900

    Controller

---
+++ src/main/java/com/example/demo/controller/CountryController.java
@@ -0,0 +1,75 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CountryController {
+	
+	@GetMapping("/USA")
+	public String UsaView() {	
+		return "usa";
+	}
+	
+	@GetMapping("/EGY")
+	public String EgyptView() {	
+		return "egy";
+	}
+	
+	@GetMapping("/AUS")
+	public String AustrariaView() {	
+		return "aus";
+	}
+	
+	@GetMapping("/IRN")
+	public String IranView() {	
+		return "irn";
+	}
+	
+	
+	@GetMapping("/ITA")
+	public String ItalyView() {	
+		return "ita";
+	}
+	
+	@GetMapping("/FRA")
+	public String FranceView() {	
+		return "fra";
+	}
+	
+	@GetMapping("/VNM")
+	public String VietnamView() {	
+		return "vnm";
+	}
+	
+	@GetMapping("/CHE")
+	public String SwitzerlandView() {	
+		return "che";
+	}
+	
+	@GetMapping("/THA")
+	public String ThaiView() {	
+		return "tha";
+	}
+	
+	@GetMapping("/IND")
+	public String IndiaView() {	
+		return "ind";
+	}
+	
+	@GetMapping("/CHN")
+	public String ChinaView() {	
+		return "chn";
+	}
+	
+	@GetMapping("/RUS")
+	public String RussiaView() {	
+		return "rus";
+	}
+	
+	@GetMapping("/KOR")
+	public String KoreaView() {	
+		return "kor";
+	}
+
+}
