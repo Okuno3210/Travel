d0a515dffdb9063f83be919147a50d93fb04d2cb
3030930 <3030930@30309-30>
2025-11-07 15:56:48 +0900

    html追加しました

--- src/main/java/com/example/demo/controller/GourmetController.java
+++ src/main/java/com/example/demo/controller/GourmetController.java
@@ -5,14 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GourmetController {
-    @GetMapping("/gourmet")
+	
+	@GetMapping("/gourmet")
     public String showGourmetPage() {
-        return "/grume/gourmet"; // templates/gourmet.html が必要
+        return "grume/gourmet"; // ← templates/grume/meat.html に対応
     }
-    
-    @GetMapping("/gourmet2")
-    public String showGourmet2Page() {
-        return "/grume/gourmet2"; // templates/gourmet.html が必要
+
+    // /grume/meat にアクセスしたら templates/grume/meat.html を表示
+    @GetMapping("/grume/meat")
+    public String showMeatPage() {
+        return "grume/meat"; // ← templates/grume/meat.html に対応
+    }
+    // /grume/meat にアクセスしたら templates/grume/meat.html を表示
+    @GetMapping("/grume/ohters")
+    public String showOhtersPage() {
+        return "grume/ohters"; // ← templates/grume/meat.html に対応
+    }
+    // /grume/meat にアクセスしたら templates/grume/meat.html を表示
+    @GetMapping("/grume/seafood")
+    public String showSeafoodPage() {
+        return "grume/seafood"; // ← templates/grume/meat.html に対応
+    }
+    // /grume/meat にアクセスしたら templates/grume/meat.html を表示
+    @GetMapping("/grume/vegetable")
+    public String showVegetablePage() {
+        return "grume/vegetable"; // ← templates/grume/meat.html に対応
+    }
+    // /grume/meat にアクセスしたら templates/grume/meat.html を表示
+    @GetMapping("/grume/grains")
+    public String showGrainsPage() {
+        return "grume/grains"; // ← templates/grume/meat.html に対応
     }
 }
-
