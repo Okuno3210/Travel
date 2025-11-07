258e7c8fedf26e3626acd6749bfb9381b0cd1519
3030928 <3030928@30309-28>
2025-11-07 15:41:16 +0900

    データ8こ追加

---
+++ src/main/java/com/example/demo/entity/TourEntity.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Tour")
+public class TourEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;          // CSVのid（DB管理用PK）
+    private Long countryId;
+    private String title;
+    private String description;
+    private String basePrice;
+    private String schedule;
+    private String imgUrl;
+    
+    
+    }
---
+++ src/main/java/com/example/demo/service/TourDataLoader.java
@@ -0,0 +1,62 @@
+package com.example.demo.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.entity.TourEntity;
+import com.example.demo.repository.TourRepository;
+
+
+
+@Component
+public class TourDataLoader implements ApplicationRunner {
+	private final TourRepository tourRepo;
+	public TourDataLoader(
+			TourRepository tourRepo) {
+			this.tourRepo = tourRepo;}
+	 @Value("classpath:data/tour.csv")
+	    private Resource tourCsv;
+	 private void loadCsv() {
+	        loadTour();}
+	 
+	 private void loadTour() {
+	        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+	                tourCsv.getInputStream(), StandardCharsets.UTF_8))) {
+
+	            br.lines().skip(1).filter(line -> !line.trim().isEmpty())
+	                    .forEach(line -> {
+	                        String[] arr = line.split(",");
+	                        // if (arr.length < 7) return;
+
+	                        TourEntity to = new TourEntity();
+	                        to.setCountryId(Long.parseLong(arr[1]));
+	                        to.setTitle(arr[2]);
+	                        to.setDescription(arr[3]);
+	                        to.setBasePrice(arr[4]);
+	                        to.setSchedule(arr[5]);
+	                        to.setImgUrl(arr[6]);
+	                        tourRepo.save(to);
+	                        
+	                    });
+	        } catch (IOException e) { e.printStackTrace(); }}
+	 
+	// アプリ起動時処理
+	 @Transactional
+	    @Override
+	    public void run(ApplicationArguments args) throws Exception {
+	        // 一旦全削除して再ロード（毎回最新化）
+		 tourRepo.deleteAllInBatch();
+		 loadCsv();}
+
+
+}
