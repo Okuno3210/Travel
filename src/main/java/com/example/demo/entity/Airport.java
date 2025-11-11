f7821ef63be3a4cae1adc0df7111f44185285e39
3030929 <3030929@30309-29>
2025-11-11 10:19:03 +0900

    各ページからの空港検索の自動化

--- src/main/java/com/example/demo/entity/Airport.java
+++ src/main/java/com/example/demo/entity/Airport.java
@@ -1,9 +1,12 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import lombok.Data;
@@ -18,7 +21,15 @@ public class Airport {
 
     private String name;   // 成田国際空港など
     private String code;   // NRT, LAXなど
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region; 
 
-    private String country;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country; 
+    
+    private String description;
 }
 
